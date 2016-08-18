daf23a9c89ecc1af65245b4b6506b32dc856c226
Alexander Kawrykow <azzro.boy@gmail.com>
2016-08-18 13:39:52 -0700

    Handle login deeplink

--- app/components/login.js
+++ app/components/login.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Linking, View } from 'react-native';
+import { AsyncStorage, Linking, View } from 'react-native';
 import Button from 'react-native-button';
 import { connect } from 'react-redux';
+import qs from 'qs';
+import { Actions } from 'react-native-router-flux'
 
 const LoginLayout = React.createClass({
-  componentDidMount() {
+  componentDidMount(){
     Linking.addEventListener('url', this._handleOpenURL);
+
+    AsyncStorage.getItem('access_token', (error, token) => {
+      if (token){
+        Actions.main();
+      }
+    });
   },
 
-  componentWillUnmount() {
+  componentWillUnmount(){
     Linking.removeEventListener('url', this._handleOpenURL);
   },
 
-  _handleOpenURL(event) {
-    console.log(event.url);
+  _handleOpenURL(event){
+    let url = new URL(event.url);
+    let path = url.pathname.substring(2); // chop off initial '//'
+    if (path === 'login'){
+      let params = url.search.substring(1); // chop off initial '?'
+      let data = qs.parse(params);
+
+      AsyncStorage.setItem('access_token', data.access_token, (error) => {
+        Actions.main();
+      });
+    }
   },
 
   render(){
