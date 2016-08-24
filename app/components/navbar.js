23c3ed565b703ee936baf6492bd8ce020b4e618f
Alexander Kawrykow <azzro.boy@gmail.com>
2016-08-23 18:28:58 -0700

    Rendering coins text on navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { NavBar } from 'react-native-router-flux';
 
+class CustomNavBar extends NavBar {
+  renderRightButton(){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-end' }}>
+        <Text>{this.props.coins}</Text>
+      </View>
+    );
+  }
+}
+
 class NavBarLayout extends Component {
   render(){
-    return <NavBar {...this.props} getTitle={() => this.props.title}/>;
+    return (
+      <CustomNavBar 
+        {...this.props}
+        getTitle={() => this.props.title} />
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    title: state.navTitle
+    title: state.navTitle,
+    coins: 500
   };
 };
 
