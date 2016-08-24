07e8ee459930e427226e4a6549a82fe32e036426
Alexander Kawrykow <azzro.boy@gmail.com>
2016-08-24 16:15:04 -0400

    Hooking up purchasing followers

---
+++ app/actions/followers.js
@@ -0,0 +1,28 @@
+import { API_HOST } from '../constants';
+import { getCoins } from './user';
+import qs from 'qs';
+
+export const purchaseFollowers = (followers) => {
+  return (dispatch, getState) => {
+    let { access_token } = getState().login;
+    if (!access_token){
+      return;
+    }
+
+    fetch(API_HOST + '/followers/get?' + qs.stringify({ access_token, followers }), {method: 'POST'})
+      .then(response => response.json().catch(err => {}))
+      .then(data => {
+        dispatch(purchasedFollowers(data));
+        dispatch(getCoins());
+      })
+      .catch(err => console.log(err));
+  };
+};
+
+export const PURCHASED_FOLLOWERS = 'PURCHASED_FOLLOWERS';
+export const purchasedFollowers = (data) => {
+  return {
+    type: PURCHASED_FOLLOWERS,
+    ...data
+  };
+};
--- app/components/getfollowers.js
+++ app/components/getfollowers.js
@@ -3,9 +3,11 @@ import { Image, View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import Button from 'react-native-button';
 
+import { purchaseFollowers } from '../actions/followers';
+
 class GetFollowersLayout extends Component {
   render(){
-    let { profile_image, followers, following } = this.props;
+    let { profile_image, followers, following, dispatch } = this.props;
 
     return (
       <View style={styles.view}>
@@ -19,7 +21,7 @@ class GetFollowersLayout extends Component {
             [20, 60, 200, 600, 1000, 2000, 6000].map(val => (
               <View key={'follower-row-' + val}>
                 <Text>+{val} followers</Text>
-                <Button>{val * 10}</Button>
+                <Button onPress={() => dispatch(purchaseFollowers(val))}>{val * 10}</Button>
               </View>
             ))
           }
