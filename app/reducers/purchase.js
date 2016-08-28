c8f96134281dffa69a98f54f988322f428af063c
Alexander Kawrykow <azzro.boy@gmail.com>
2016-08-27 21:18:40 -0400

    Sort products by price ascending

--- app/reducers/purchase.js
+++ app/reducers/purchase.js
@@ -3,7 +3,7 @@ import { LOADED_PRODUCTS } from '../actions/purchase';
 export const products = (state = [], action) => {
   switch (action.type){
     case LOADED_PRODUCTS:
-      return action.products;
+      return action.products.sort(function(a, b){ return a.price - b.price });
     default:
       return state;
   }
