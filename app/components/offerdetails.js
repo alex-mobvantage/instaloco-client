671b027f181bee15dd886828a4ac7e9b7088eb6b
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-02 09:15:25 -0700

    Fleshing out basic layout of offer details page

--- app/components/offerdetails.js
+++ app/components/offerdetails.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
+import Button from 'react-native-button';
 
 import NavBar from './navbar';
 
 class OfferDetailsLayout extends Component {
   render(){
+    let { id, title, points, image, description } = this.props;
+
     return (
-      <View style={styles.view}>
-        <Text style={styles.text}>Offer details</Text>
+      <View style={{flex: 1, marginTop: 70}}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <Image source={{uri: image}} style={{width: 50, height: 50}} />
+            <Text>{title}</Text>
+          </View>
+          <View>
+            <Text>{points}</Text>
+          </View>
+        </View>
+        <Text>{description}</Text>
+        <Button>Download</Button>
+        <Text>To Get Your Points...</Text>
+        
+        <Text>Download and test the app for 30-60 seconds</Text>
+        <Text>Complete any tutorials</Text>
+
+        <Text>Do not switch networks</Text>
+        <Text>Like switching from WiFi to 3G</Text>
+
+        <Text>Can I remove the app from my device?</Text>
+        <Text>Yep - be sure you have earned your points first</Text>
       </View>
     );
   }
@@ -17,18 +40,5 @@ class OfferDetailsLayout extends Component {
     return <NavBar {...navProps} title='Free Coins' />;
   }
 }
-
-const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    color: '#000000'
-  },
-  view: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
 
 export default OfferDetails = connect()(OfferDetailsLayout);
