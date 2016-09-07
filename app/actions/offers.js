3b8a660c8eefe1658d8af9f74a36a9761c3e0cd9
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-07 12:55:41 -0700

    Requesting push permissions for first offer

--- app/actions/offers.js
+++ app/actions/offers.js
@@ -1,6 +1,7 @@
 import { API_HOST } from '../constants';
 import qs from 'qs';
-import { Linking } from 'react-native';
+import { Alert, AsyncStorage, Linking, PushNotificationIOS } from 'react-native';
+import Promise from 'bluebird';
 
 export const fetchOffers = () => {
   return (dispatch, getState) => {
@@ -53,7 +54,8 @@ export const beginOffer = (offer_id, redirect_url) => {
       return;
     }
 
-    fetch(API_HOST + '/offers/begin?' + qs.stringify({ access_token, offer_id }), {method: 'POST'})
+    requestPushPermissions()
+      .then(() => fetch(API_HOST + '/offers/begin?' + qs.stringify({ access_token, offer_id }), {method: 'POST'}))
       .then(response => response.json().catch(err => {}))
       .then(data => {
         Linking.openURL(redirect_url);
@@ -62,6 +64,43 @@ export const beginOffer = (offer_id, redirect_url) => {
       })
       .catch(err => console.log(err));
   };
+};
+
+const requestPushPermissions = () => {
+  return AsyncStorage.getItem('push_notifications_requested')
+    .then(requested => {
+      if (requested === null){
+        return new Promise((success, error) => {
+          Alert.alert(
+            null,
+            'Some offers may take a lil\' while to complete. Would you like us to notify you when your coins have arrived?',
+            [
+              {text: 'OK', onPress: () => success(true)},
+              {text: 'No thanks', onPress: () => success(false)}
+            ]
+          )
+        })
+        .then(permission => {
+          if (permission){
+            PushNotificationIOS.requestPermissions({alert: true})
+
+            return Promise.delay(5 * 1000)
+              .then(() => new Promise((success) => PushNotificationIOS.checkPermissions(success)))
+              .then(permissions => {
+                if (permissions.alert){
+                  return AsyncStorage.setItem('push_notifications_enabled', 'true');
+                } else {
+                  return AsyncStorage.setItem('push_notifications_enabled', 'false');
+                }
+              })
+          } else {
+            return AsyncStorage.setItem('push_notifications_enabled', 'false');
+          }
+        })
+        .then(() => AsyncStorage.setItem('push_notifications_requested', 'true'))
+        .catch(err => console.log('error requesting permissions', err));
+      }
+    });
 };
 
 export const BEGAN_OFFER = 'BEGAN_OFFER';
