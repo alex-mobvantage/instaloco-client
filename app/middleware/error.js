0598f5bc0a7209255bcbe2bc3950482cfba5091d
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-15 13:31:06 -0700

    Fixing error middleware error message formatting

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -2,7 +2,7 @@ import { Alert } from 'react-native';
 
 export default errorMiddleware = store => next => action => {
   if (action.error){
-    Alert.alert(null, action.error);
+    Alert.alert('Error', action.error);
     return;
   }
 
