791dd8144cc163d4f2c8e71402d0b42a63275427
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-15 19:09:18 -0700

    Fix loading screen not disappearing on error

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -3,7 +3,6 @@ import { Alert } from 'react-native';
 export default errorMiddleware = store => next => action => {
   if (action.error){
     Alert.alert('Error', action.error);
-    return;
   }
 
   return next(action);
