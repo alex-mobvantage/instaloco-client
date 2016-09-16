105e7a7e8de5c63b1b0e5331579d6eaa2573e16a
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-16 14:11:43 -0700

    Basic styling of more page

--- app/components/more.js
+++ app/components/more.js
@@ -6,31 +6,59 @@ import { Actions } from 'react-native-router-flux';
 
 import { invalidateAccessToken } from '../actions/auth';
 
+import * as commonStyles from '../styles/common';
+import * as colors from '../styles/colors';
+
 class MoreLayout extends Component {
   render(){
     let { dispatch } = this.props;
 
     return (
-      <View style={styles.view}>
-        <Button onPress={() => Actions.faq()}>FAQ</Button>
-        <Button>Support</Button>
-        <Button onPress={() => Actions.legal()}>Legal</Button>
-        <Button onPress={() => dispatch(invalidateAccessToken())}>Logout</Button>
+      <View style={[commonStyles.containers.base, commonStyles.containers.centered, commonStyles.containers.tabbed, styles.container]}>
+        <View style={commonStyles.containers.list}>
+          <View style={[commonStyles.containers.listItem, styles.listItem]}>
+            <Button
+              style={[commonStyles.fonts.base, commonStyles.fonts.button, styles.button]}
+              onPress={() => Actions.faq()}>
+              FAQ
+            </Button>
+          </View>
+          <View style={[commonStyles.containers.listItem, styles.listItem]}>
+            <Button
+              style={[commonStyles.fonts.base, commonStyles.fonts.button, styles.button]}>
+              Support
+            </Button>
+          </View>
+          <View style={[commonStyles.containers.listItem, styles.listItem]}>
+            <Button
+              style={[commonStyles.fonts.base, commonStyles.fonts.button, styles.button]}
+              onPress={() => Actions.legal()}>
+              Legal
+            </Button>
+          </View>
+          <View style={[commonStyles.containers.listItem, styles.listItem]}>
+            <Button
+              style={[commonStyles.fonts.base, commonStyles.fonts.button, styles.button]}
+              onPress={() => dispatch(invalidateAccessToken())}>
+              Logout
+            </Button>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    textAlign: 'center',
-    color: '#000000'
+  container: {
+    backgroundColor: colors.primary
   },
-  view: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    alignItems: 'center',
-    justifyContent: 'center'
+  listItem: {
+    justifyContent: 'center',
+    padding: 12
+  },
+  button: {
+    fontSize: 19
   }
 });
 
