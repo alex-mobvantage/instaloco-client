42be70a4bbf47c2c31bce8ea2b44130ef423deeb
Alexander Kawrykow <azzro.boy@gmail.com>
2016-09-23 14:26:07 -0700

    Remove deeplink handler for login

--- app/components/login.js
+++ app/components/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AsyncStorage, Linking, View } from 'react-native';
+import { AsyncStorage, View } from 'react-native';
 import Button from 'react-native-button';
 import { connect } from 'react-redux';
 import qs from 'qs';
@@ -9,26 +9,8 @@ import { loadAccessToken, saveAccessToken } from '../actions/auth';
 
 const LoginLayout = React.createClass({
   componentDidMount(){
-    Linking.addEventListener('url', this._handleOpenURL);
-    
     let { dispatch } = this.props;
     dispatch(loadAccessToken());
-  },
-
-  componentWillUnmount(){
-    Linking.removeEventListener('url', this._handleOpenURL);
-  },
-
-  _handleOpenURL(event){
-    // TODO: Harden this 
-    let path = event.url.substring('likesforapps://'.length, event.url.indexOf('?'));
-    if (path === 'login'){
-      let params = event.url.substring(event.url.indexOf('?') + 1);
-      let data = qs.parse(params);
-
-      let { dispatch } = this.props;
-      dispatch(saveAccessToken(data.access_token));
-    }
   },
 
   render(){
