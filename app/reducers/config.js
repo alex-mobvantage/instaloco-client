9a3e84df986a9709b7e39f20cb1cc7d34f95595e
Alexander Kawrykow <azzro.boy@gmail.com>
2016-10-17 12:04:45 -0700

    Add default values for coins_per_like and coins_per_follower

--- app/reducers/config.js
+++ app/reducers/config.js
@@ -1,6 +1,6 @@
 import { CONFIG_RECEIVED } from '../actions/config';
 
-export const config = (state = {}, action) => {
+export const config = (state = {coins_per_like: 2, coins_per_follower: 10}, action) => {
   switch (action.type){
     case CONFIG_RECEIVED:
       return action.config;
